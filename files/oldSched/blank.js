0b7f19012cec0214126c7ce1305d688b7562a2e7
Richard Williams <rkwilli20204@gmail.com>
2026-08-16 16:56:05 -0400

    fall sched update

---
+++ files/oldSched/blank.js
@@ -0,0 +1,178 @@
+const scheduleData = {
+	title: "Weekly Schedule Spring 2026",
+
+	days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
+
+	rows: {
+		"8:00am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"8:30am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched"	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"9:00am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"9:30am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",        type: "sched" 	}
+		},
+		"10:00am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}
+		},
+		"10:30am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"11:00am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched"	},
+			"Wednesday": { text: "",	type: "sched"	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched"	}	
+		},
+		"11:30am": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched"	},
+			"Wednesday": { text: "",	type: "sched"	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched"	}	
+		},
+		"12:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"12:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"1:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"1:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"2:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"2:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"3:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"3:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"4:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"4:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"5:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"5:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"6:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"6:30pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		},
+		"7:00pm": {
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "",	type: "sched" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
+		}
+	},
+
+	importantDates: [
+		"(May 18-22 2026. Campinas, Brazil) Workshop on Dynamic Equations on Time Scales, Difference Equations, and Applications in Mathematical Biology, in honor of Martin Bohner",
+		"(July 13-17 2026. Milan, Italy) 31st International Conference on Difference Equations and Applications"
+	],
+
+	archived: [
+
+	]
+};
--- files/schedule.js
+++ files/schedule.js
@@ -1,5 +1,5 @@
 const scheduleData = {
-	title: "Weekly Schedule Spring 2026",
+	title: "Weekly Schedule Fall 2026",
 
 	days: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
 
@@ -13,141 +13,141 @@ const scheduleData = {
 		},
 		"8:30am": {
 			"Monday":    { text: "",	type: "sched" 	},
-			"Tuesday":   { text: "AM",	type: "help"	},
+			"Tuesday":   { text: "",	type: "sched"	},
 			"Wednesday": { text: "",	type: "sched" 	},
 			"Thursday":  { text: "",	type: "sched" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"9:00am": {
 			"Monday":    { text: "",	type: "sched" 	},
-			"Tuesday":   { text: "AM",	type: "help" 	},
-			"Wednesday": { text: "LC",	type: "help" 	},
-			"Thursday":  { text: "",	type: "sched" 	},
-			"Friday":    { text: "",	type: "sched" 	}	
+			"Tuesday":   { text: "Office",	type: "help" 	},
+			"Wednesday": { text: "MTH 589",	type: "class" 	},
+			"Thursday":  { text: "Office",	type: "help" 	},
+			"Friday":    { text: "C",	type: "meeting" }	
 		},
 		"9:30am": {
 			"Monday":    { text: "",	type: "sched" 	},
-			"Tuesday":   { text: "STA 546", type: "class" 	},
-			"Wednesday": { text: "LC",      type: "help"  	},
-			"Thursday":  { text: "STA 546", type: "class" 	},
-			"Friday":    { text: "",        type: "sched" 	}
+			"Tuesday":   { text: "Office",	type: "help" 	},
+			"Wednesday": { text: "MTH 589",	type: "class" 	},
+			"Thursday":  { text: "Office",	type: "help" 	},
+			"Friday":    { text: "C",	type: "meeting" }
 		},
 		"10:00am": {
-			"Monday":    { text: "C",       type: "meeting"	},
-			"Tuesday":   { text: "STA 546", type: "class"	},
-			"Wednesday": { text: "AE",      type: "help"	},
-			"Thursday":  { text: "STA 546", type: "class"	},
-			"Friday":    { text: "AE",      type: "help"	}
+			"Monday":    { text: "MTH 102",	type: "teach" 	},
+			"Tuesday":   { text: "MTH 102",	type: "teach" 	},
+			"Wednesday": { text: "MTH 102",	type: "teach" 	},
+			"Thursday":  { text: "MTH 102",	type: "teach" 	},
+			"Friday":    { text: "C",	type: "meeting" }
 		},
 		"10:30am": {
-			"Monday":    { text: "C",	type: "meeting"	},
-			"Tuesday":   { text: "STA 546",	type: "class"	},
-			"Wednesday": { text: "AE",	type: "help"	},
-			"Thursday":  { text: "STA 546",	type: "class"	},
-			"Friday":    { text: "AE",	type: "help"	}	
+			"Monday":    { text: "MTH 102",	type: "teach" 	},
+			"Tuesday":   { text: "MTH 102",	type: "teach" 	},
+			"Wednesday": { text: "MTH 102",	type: "teach" 	},
+			"Thursday":  { text: "MTH 102",	type: "teach" 	},
+			"Friday":    { text: "C",	type: "meeting" }	
 		},
 		"11:00am": {
-			"Monday":    { text: "C",	type: "meeting"	},
-			"Tuesday":   { text: "",	type: "sched"	},
+			"Monday":    { text: "ND",	type: "help" 	},
+			"Tuesday":   { text: "TB",	type: "help" 	},
 			"Wednesday": { text: "",	type: "sched"	},
-			"Thursday":  { text: "MM",	type: "help"	},
+			"Thursday":  { text: "DS",	type: "help" 	},
 			"Friday":    { text: "",	type: "sched"	}	
 		},
 		"11:30am": {
-			"Monday":    { text: "C",	type: "meeting"	},
-			"Tuesday":   { text: "",	type: "sched"	},
+			"Monday":    { text: "ND",	type: "help" 	},
+			"Tuesday":   { text: "TB",	type: "help" 	},
 			"Wednesday": { text: "",	type: "sched"	},
-			"Thursday":  { text: "MM",	type: "help"	},
+			"Thursday":  { text: "DS",	type: "help" 	},
 			"Friday":    { text: "",	type: "sched"	}	
 		},
 		"12:00pm": {
-			"Monday":    { text: "MTH 635",	type: "class" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "MTH 635",	type: "class" 	},
-			"Thursday":  { text: "",	type: "sched" 	},
-			"Friday":    { text: "MTH 635",	type: "class" 	}	
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "Lunch",	type: "nosched" },
+			"Wednesday": { text: "LR",	type: "help" 	},
+			"Thursday":  { text: "Lunch",	type: "nosched" },
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"12:30pm": {
-			"Monday":    { text: "MTH 635",	type: "class" 	},
-			"Tuesday":   { text: "MTH 615",	type: "class" 	},
-			"Wednesday": { text: "MTH 635",	type: "class" 	},
-			"Thursday":  { text: "MTH 615",	type: "class" 	},
-			"Friday":    { text: "MTH 635",	type: "class" 	}	
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "STA 661",	type: "class" 	},
+			"Wednesday": { text: "LR",	type: "help" 	},
+			"Thursday":  { text: "STA 661",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"1:00pm": {
-			"Monday":    { text: "JH",	type: "help" 	},
-			"Tuesday":   { text: "MTH 615",	type: "class" 	},
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "STA 661",	type: "class" 	},
 			"Wednesday": { text: "",	type: "sched" 	},
-			"Thursday":  { text: "MTH 615",	type: "class" 	},
-			"Friday":    { text: "JH",	type: "help" 	}	
+			"Thursday":  { text: "STA 661",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"1:30pm": {
-			"Monday":    { text: "JH",	type: "help" 	},
-			"Tuesday":   { text: "MTH 615",	type: "class" 	},
+			"Monday":    { text: "",	type: "sched" 	},
+			"Tuesday":   { text: "STA 661",	type: "class" 	},
 			"Wednesday": { text: "",	type: "sched" 	},
-			"Thursday":  { text: "MTH 615",	type: "class" 	},
-			"Friday":    { text: "JH",	type: "help" 	}	
+			"Thursday":  { text: "STA 661",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"2:00pm": {
-			"Monday":    { text: "MM",	type: "help" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "",	type: "sched" 	},
-			"Thursday":  { text: "MB",	type: "help" 	},
-			"Friday":    { text: "DW/AD",	type: "help" 	}	
+			"Monday":    { text: "MTH 670",	type: "class" 	},
+			"Tuesday":   { text: "LJ",	type: "help" 	},
+			"Wednesday": { text: "AG",	type: "help" 	},
+			"Thursday":  { text: "MTH 670",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"2:30pm": {
-			"Monday":    { text: "MM",	type: "help" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "S",	type: "meeting"	},
-			"Thursday":  { text: "MB",	type: "help" 	},
-			"Friday":    { text: "DW/AD",	type: "help" 	}	
+			"Monday":    { text: "MTH 670",	type: "class" 	},
+			"Tuesday":   { text: "LJ",	type: "help" 	},
+			"Wednesday": { text: "AG",	type: "help" 	},
+			"Thursday":  { text: "MTH 670",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"3:00pm": {
-			"Monday":    { text: "",	type: "sched" 	},
+			"Monday":    { text: "Lab",	type: "priv_t" 	},
 			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "",	type: "sched" 	},
-			"Thursday":  { text: "LC",	type: "help" 	},
-			"Friday":    { text: "GS",	type: "help" 	}	
+			"Wednesday": { text: "Lab",	type: "priv_t" 	},
+			"Thursday":  { text: "MTH 670",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"3:30pm": {
-			"Monday":    { text: "LM",	type: "help" 	},
-			"Tuesday":   { text: "LM",	type: "help" 	},
-			"Wednesday": { text: "LM",	type: "help" 	},
-			"Thursday":  { text: "LC",	type: "help" 	},
-			"Friday":    { text: "GS",	type: "help" 	}	
+			"Monday":    { text: "Lab",	type: "priv_t" 	},
+			"Tuesday":   { text: "MTH 643",	type: "class" 	},
+			"Wednesday": { text: "Lab",	type: "priv_t" 	},
+			"Thursday":  { text: "MTH 643",	type: "class" 	},
+			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"4:00pm": {
-			"Monday":    { text: "LM",	type: "help" 	},
-			"Tuesday":   { text: "LM",	type: "help" 	},
-			"Wednesday": { text: "LM",	type: "help" 	},
-			"Thursday":  { text: "CF",	type: "priv_t" 	},
+			"Monday":    { text: "Lab",	type: "priv_t" 	},
+			"Tuesday":   { text: "MTH 643",	type: "class" 	},
+			"Wednesday": { text: "Lab",	type: "priv_t" 	},
+			"Thursday":  { text: "MTH 643",	type: "class" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"4:30pm": {
-			"Monday":    { text: "",	type: "sched" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "AM",	type: "help" 	},
-			"Thursday":  { text: "CF",	type: "priv_t" 	},
+			"Monday":    { text: "Lab",	type: "priv_t" 	},
+			"Tuesday":   { text: "MTH 643",	type: "class" 	},
+			"Wednesday": { text: "Lab",	type: "priv_t" 	},
+			"Thursday":  { text: "MTH 643",	type: "class" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"5:00pm": {
-			"Monday":    { text: "LJ",	type: "help" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "AM",	type: "help" 	},
-			"Thursday":  { text: "LJ",	type: "help" 	},
+			"Monday":    { text: "CA",	type: "help" 	},
+			"Tuesday":   { text: "DB",	type: "help" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"5:30pm": {
-			"Monday":    { text: "LJ",	type: "help" 	},
-			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "GS",	type: "help" 	},
-			"Thursday":  { text: "LJ",	type: "help" 	},
+			"Monday":    { text: "CA",	type: "help" 	},
+			"Tuesday":   { text: "DB",	type: "help" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
+			"Thursday":  { text: "",	type: "sched" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
 		"6:00pm": {
 			"Monday":    { text: "",	type: "sched" 	},
 			"Tuesday":   { text: "",	type: "sched" 	},
-			"Wednesday": { text: "GS",	type: "help" 	},
+			"Wednesday": { text: "",	type: "sched" 	},
 			"Thursday":  { text: "",	type: "sched" 	},
 			"Friday":    { text: "",	type: "sched" 	}	
 		},
@@ -168,8 +168,7 @@ const scheduleData = {
 	},
 
 	importantDates: [
-		"(May 18-22 2026. Campinas, Brazil) Workshop on Dynamic Equations on Time Scales, Difference Equations, and Applications in Mathematical Biology, in honor of Martin Bohner",
-		"(July 13-17 2026. Milan, Italy) 31st International Conference on Difference Equations and Applications"
+		
 	],
 
 	archived: [
